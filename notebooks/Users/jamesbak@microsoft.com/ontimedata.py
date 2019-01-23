80b1b0b66499b179c5e58baa509aace9a42b8b3f
James Baker <jamesbak@microsoft.com>
2019-01-22 17:09:57 -0800

    Update: LICENSE notebooks

---
+++ notebooks/Users/jamesbak@microsoft.com/ontimedata.py
@@ -0,0 +1,55 @@
+# Databricks notebook source
+basePath = "/mnt/ontimedata/"
+baseAbfsPath="abfss://ontimedata@adlsgen2demo2.dfs.core.windows.net/"
+configs = {"fs.azure.account.auth.type": "OAuth",
+           "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+           "fs.azure.account.oauth2.client.id": "27142561-761b-4a54-89c1-cf56e577ca38",
+           "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope = "adlsgen2demo", key = "spn-secret"),
+           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/microsoft.com/oauth2/token"}
+
+# Optionally, you can add <your-directory-name> to the source URI of your mount point.
+dbutils.fs.mount(
+  source = baseAbfsPath,
+  mount_point = basePath,
+  extra_configs = configs)
+
+
+# COMMAND ----------
+
+for tableName in ["OnTimeFacts", "metar", "aircraft"]:
+  df = spark.read.format('csv').options(header='true', inferschema='true').load(basePath + "csv/" + tableName)
+  df.toDF(*[col.strip() for col in df.columns]).write.saveAsTable(tableName,
+      format='parquet',
+      mode='overwrite',
+      path=baseAbfsPath + 'parquet/' + tableName)
+
+# COMMAND ----------
+
+df.toDF(*[col.strip() for col in df.columns])
+
+
+# COMMAND ----------
+
+basePath = "/mnt/ontimedata/"
+baseAbfsPath="abfss://ontimedata@adlsgen2demo2.dfs.core.windows.net/"
+for dimension in dbutils.fs.ls(basePath + "csv/dimensions"):
+  df = spark.read.format('csv').options(header='true', inferschema='true').load(dimension.path)
+  df.write.saveAsTable('Dim_' + dimension.name.strip('/'), format='parquet', mode='overwrite', path=baseAbfsPath + 'parquet/dimensions/' + dimension.name)
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC create table deltatest
+# MAGIC (code string, description string)
+# MAGIC using delta
+# MAGIC location "abfss://ontimedata@adlsgen2demo2.dfs.core.windows.net/parquet/dimensions/airports"
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * 
+# MAGIC from OnTimeFacts
+# MAGIC where NASDelay != 0
+# MAGIC limit 20
